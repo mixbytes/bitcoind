0c88795468d0859b86ec31bcd74b0edd1a5141f4
Mikhail Shubin <mikhail.shubin@gmail.com>
2017-12-21 18:28:48 +0300

    basic tests

--- test/tests.sh
+++ test/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 sudo docker build -t bitcoind:testing .
-sudo docker run -d -p 8332:8332 -p 8333:8333 -e RPCALLOWIP="0.0.0.0/0" -e RPCUSER=user RPCPASS=pass bitcoind:testing
+sudo docker run -d -p 8332:8332 -p 8333:8333 -e RPCALLOWIP="0.0.0.0/0" -e RPCUSER=user -e RPCPASS=pass bitcoind:testing
 sleep 60
 curl --user user:pass --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:8332
 
