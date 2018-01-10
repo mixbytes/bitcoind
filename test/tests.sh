27c0ffe4799c2487ea95c2c4e69e22009e26b88c
Mikhail Shubin <mikhail.shubin@gmail.com>
2018-01-10 09:40:52 +0300

    goss test

--- test/tests.sh
+++ test/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 sudo docker build -t bitcoind:testing .
-sudo curl -fsSL https://goss.rocks/install | sh
+curl -fsSL https://goss.rocks/install | sudo sh
 sudo GOSS_SLEEP=10 dgoss run -p 8332:8332 -p 8333:8333 -e RPCALLOWIP="0.0.0.0/0" -e RPCUSER=user -e RPCPASS=pass bitcoind:testing
 sudo docker run -d -p 8332:8332 -p 8333:8333 -e RPCALLOWIP="0.0.0.0/0" -e RPCUSER=user -e RPCPASS=pass bitcoind:testing
 sleep 60
