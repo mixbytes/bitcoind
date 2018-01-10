23687e36f84b8f74672e6221c5d525c5d3e9ecf5
Mikhail Shubin <mikhail.shubin@gmail.com>
2018-01-10 10:58:19 +0300

    tests improvement

--- test/tests.sh
+++ test/tests.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
+
+RPCTESTUSER="user"
+RPCTESTPASS="pass"
+
 set -ex
 sudo docker build -t bitcoind:testing .
-curl -fsSL https://goss.rocks/install | sudo sh
-sudo GOSS_SLEEP=10 dgoss run -p 8332:8332 -p 8333:8333 -e RPCALLOWIP="0.0.0.0/0" -e RPCUSER=user -e RPCPASS=pass bitcoind:testing
-sudo docker run -d -p 8332:8332 -p 8333:8333 -e RPCALLOWIP="0.0.0.0/0" -e RPCUSER=user -e RPCPASS=pass bitcoind:testing
+sudo GOSS_SLEEP=10 dgoss run -p 8332:8332 -p 8333:8333 -e RPCALLOWIP="0.0.0.0/0" -e RPCUSER="$RPCTESTUSER" -e RPCPASS="$RPCTESTPASS" bitcoind:testing
+
+sed -i "s/RPCUSER:.*/RPCUSER: \"$RPCTESTUSER\"/" docker-compose.yml
+sed -i "s/RPCPASS:.*/RPCPASS: \"$RPCTESTPASS\"/" docker-compose.yml
+sed -i 's/RPCALLOWIP:.*/RPCALLOWIP: "0.0.0.0\/0"/' docker-compose.yml
+cat docker-compose.yml
+
+#sudo docker run -d -p 8332:8332 -p 8333:8333 -e RPCALLOWIP="0.0.0.0/0" -e RPCUSER=user -e RPCPASS=pass bitcoind:testing
+sudo docker-compose --build up -d
 sleep 60
 curl --user user:pass --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:8332
 
