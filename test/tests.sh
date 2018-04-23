47972e14f093d4348770187deab01948b18ab5c5
Konstantin Privezentsev <privezentsev@gmail.com>
2018-04-23 17:37:10 +0300

    set default registry for image name

--- test/tests.sh
+++ test/tests.sh
@@ -10,8 +10,9 @@ sudo GOSS_SLEEP=10 dgoss run -p 8332:8332 -p 8333:8333 -e RPCALLOWIP="0.0.0.0/0"
 sed -i "s/RPCUSER:.*/RPCUSER: \"$RPCTESTUSER\"/" docker-compose.yml
 sed -i "s/RPCPASS:.*/RPCPASS: \"$RPCTESTPASS\"/" docker-compose.yml
 sed -i 's/RPCALLOWIP:.*/RPCALLOWIP: "0.0.0.0\/0"/' docker-compose.yml
-sudo docker-compose build
-sudo docker-compose up -d
+export REGISTRY=${REGISTRY:-docker.io}
+sudo REGISTRY=${REGISTRY} docker-compose build
+sudo REGISTRY=${REGISTRY} docker-compose up -d
 sleep 60
 curl --user user:pass --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:8332
 
